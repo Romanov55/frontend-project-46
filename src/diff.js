2991104b167f9ebca00aacb11297102ce315843f
Romanov55 <Romanov20let@gmail.com>
2022-11-09 17:01:16 +0600

    delite diff

--- src/diff.js
+++
@@ -1,6 +0,0 @@
-import genDif from '@hexlet/code';
-import filepath1 from '../bin/file1.json'
-import filepath2 from '../bin/file2.json'
-
-const diff = genDif(filepath1, filepath2);
-console.log(diff);
