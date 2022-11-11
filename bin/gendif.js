77920c38ccdc0abc48f35ceb733f9f7274fdb552
Romanov55 <Romanov20let@gmail.com>
2022-11-11 12:28:12 +0600

    up gendif

--- bin/gendif.js
+++ bin/gendif.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import genDiff from "../src/index.js";
 
 const program = new Command();
 
@@ -11,9 +12,8 @@ program
   .helpOption('-h, --help', 'display help for command')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
-
