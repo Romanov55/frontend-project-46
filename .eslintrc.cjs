22110039c58891d1f7c8caa928613759459c7163
Romanov55 <Romanov20let@gmail.com>
2022-11-17 14:41:58 +0600

    up eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
+    jest: true,
   },
   extends: 'airbnb-base',
   overrides: [
@@ -11,5 +12,8 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'import/extensions': 'off'
   },
 };
