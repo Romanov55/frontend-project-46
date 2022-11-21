bfb447e483491759674b1bfc2446fdbc7d645eef
Romanov55 <Romanov20let@gmail.com>
2022-11-21 22:02:25 +0600

    up index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,8 +12,8 @@ const filepath1 = getFixturePath('file1.json');
 const filepath2 = getFixturePath('file2.json');
 const expected = readFile('trueResult.txt');
 
-const filepath3 = getFixturePath('file1.yaml');
-const filepath4 = getFixturePath('file2.yaml');
+const filepath3 = getFixturePath('file1.yml');
+const filepath4 = getFixturePath('file2.yml');
 
 test('genDiff', () => {
   expect(genDiff(filepath1, filepath2)).toEqual(expected);
