827585a86c2acc3be5e8692a0a1356f3bd1a8bd0
Romanov55 <Romanov20let@gmail.com>
2022-11-21 21:54:06 +0600

    up index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import findDiff from './findDiff.js';
-
-const readFile = (file) => {
-  const fullPath = path.resolve(process.cwd(), file);
-  const data = readFileSync(fullPath, 'utf-8');
-  return data;
-};
+import parser from './parsers.js'
 
 const genDiff = (file1, file2) => {
-  const data1 = readFile(file1);
-  const data2 = readFile(file2);
+  const data1 = parser(file1);
+  const data2 = parser(file2);
 
-  const parseData1 = JSON.parse(data1);
-  const parseData2 = JSON.parse(data2);
-  return findDiff(parseData1, parseData2);
+  return findDiff(data1, data2);
 };
 
 export default genDiff;
