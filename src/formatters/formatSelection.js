31706ecda7dcfbf3ff6b5f45d585ebf7405fdf39
Romanov55 <Romanov20let@gmail.com>
2022-11-23 17:36:47 +0600

    up format selection

---
+++ src/formatters/formatSelection.js
@@ -0,0 +1,16 @@
+import stylish from './stylish.js';
+import plain from './plain.js';
+
+const chooseFormat = (data, format = 'stylish') => {
+  if (format === 'stylish') {
+    return stylish(data)
+  }
+  if (format === 'plain') {
+    return plain(data)
+  }
+  if (format === 'json') {
+    return JSON.stringify(data)
+  }
+};
+
+export default chooseFormat;
