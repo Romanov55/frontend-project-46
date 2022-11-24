b4f18287fdab10b5f04c2b700038e4b8c9e8681b
Romanov55 <Romanov20let@gmail.com>
2022-11-24 13:28:26 +0600

    up parsers

--- src/parsers.js
+++ src/parsers.js
@@ -2,11 +2,12 @@ import yaml from 'js-yaml';
 
 const parser = (data, format) => {
   if (format === 'json') {
-    return JSON.parse(data)
+    return JSON.parse(data);
   }
   if (format === 'yaml' || format === 'yml') {
-    return yaml.load(data)
+    return yaml.load(data);
   }
+  return `Error! Unknown format - ${format}`;
 };
 
 export default parser;
