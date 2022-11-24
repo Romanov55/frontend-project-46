640f174d513b535c4e70b13bda79dad52d32ad20
Romanov55 <Romanov20let@gmail.com>
2022-11-24 12:28:24 +0600

    up findDiff

--- src/findDiff.js
+++ src/findDiff.js
@@ -7,19 +7,14 @@ const findDiff = (obj1, obj2) => {
   const result = allKeys.map((key) => {
     if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
       return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
-    }
-    if (!_.has(obj1, key)) {
+    } else if (!_.has(obj1, key)) {
       return { key, value2: obj2[key], type: 'added' };
-    }
-    if (!_.has(obj2, key)) {
+    } else if (!_.has(obj2, key)) {
       return { key, value1: obj1[key], type: 'deleted' };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
-        key, value1: obj1[key], value2: obj2[key], type: 'changed',
-      };
-    }
-    return { key, value1: obj1[key], type: 'unchanged' };
+    } 
+    return obj1[key] !== obj2[key] 
+    ? { key, value1: obj1[key], value2: obj2[key], type: 'changed', }
+    : { key, value1: obj1[key], type: 'unchanged' };
   });
   return result;
 };
