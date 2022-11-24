3ec69e07234478899f716ddddfb6e16452cce4a6
Romanov55 <Romanov20let@gmail.com>
2022-11-24 13:54:01 +0600

    up findDiff

--- src/findDiff.js
+++ src/findDiff.js
@@ -1,28 +1,32 @@
 import _ from 'lodash';
 
-const sortKeys = (obj1, obj2) => {
-  return _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-}
+const sortKeys = (obj1, obj2) => _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 
 const composeAnswer = (key, obj1, obj2) => {
   if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
     return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
-  } else if (!_.has(obj1, key)) {
+  } if (!_.has(obj1, key)) {
     return { key, value2: obj2[key], type: 'added' };
-  } else if (!_.has(obj2, key)) {
+  } if (!_.has(obj2, key)) {
     return { key, value1: obj1[key], type: 'deleted' };
-  } 
-  return obj1[key] !== obj2[key] 
-    ? { key, value1: obj1[key], value2: obj2[key], type: 'changed', }
+  }
+  return obj1[key] !== obj2[key]
+    ? {
+      key, value1: obj1[key], value2: obj2[key], type: 'changed',
+    }
     : { key, value1: obj1[key], type: 'unchanged' };
-}
+};
 
 const findDiff = (obj1, obj2) => {
-  const allKeys = sortKeys(obj1, obj2)
-  
+  const allKeys = sortKeys(obj1, obj2);
+
   const result = allKeys.map((key) => {
-    return composeAnswer(key, obj1, obj2)
-  });
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
+    }
+    return composeAnswer(key, obj1, obj2);
+  })
+  
   return result;
 };
 
