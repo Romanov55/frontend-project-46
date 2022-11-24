f8f82f2875f194cce17eec0e7eb1bcc89ce9af85
Romanov55 <Romanov20let@gmail.com>
2022-11-24 12:36:45 +0600

    up findDiff

--- src/findDiff.js
+++ src/findDiff.js
@@ -4,10 +4,8 @@ const sortKeys = (obj1, obj2) => {
   return _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 }
 
-const findDiff = (obj1, obj2) => {
-  const allKeys = sortKeys(obj1, obj2)
-  
-  const result = allKeys.map((key) => {
+const composeAnswer = (obj1, obj2, allKeys) => {
+    const answer = allKeys.map((key) => {
     if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
       return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
     } else if (!_.has(obj1, key)) {
@@ -19,7 +17,13 @@ const findDiff = (obj1, obj2) => {
     ? { key, value1: obj1[key], value2: obj2[key], type: 'changed', }
     : { key, value1: obj1[key], type: 'unchanged' };
   });
-  return result;
+  return answer;
+}
+
+const findDiff = (obj1, obj2) => {
+  const allKeys = sortKeys(obj1, obj2)
+  
+  return composeAnswer(obj1, obj2, allKeys)
 };
 
 export default findDiff;
