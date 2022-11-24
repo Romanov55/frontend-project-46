a5c82d66cad87f200e3ab9027b8e6cc45341cf55
Romanov55 <Romanov20let@gmail.com>
2022-11-24 09:16:25 +0600

    up index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import parser from './parsers.js';
 import findDiff from './findDiff.js';
-import stylish from '../src/formatters/stylish.js';
+import formatSelection from '../src/formatters/formatSelection.js';
 
 const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
 const getFormat = (filepath) => filepath.split('.')[1];
@@ -11,7 +11,7 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file1 = parser(getData(filepath1), getFormat(filepath1));
   const file2 = parser(getData(filepath2), getFormat(filepath2));
 
-  return stylish(findDiff(file1, file2), format);
+  return formatSelection(findDiff(file1, file2), format);
 };
 
 export default genDiff;
