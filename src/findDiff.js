2e696a2f80db9f227edd97adade1dd690b65fb95
Romanov55 <Romanov20let@gmail.com>
2022-11-24 13:01:16 +0600

    up findDiff

--- src/findDiff.js
+++ src/findDiff.js
@@ -4,25 +4,26 @@ const sortKeys = (obj1, obj2) => {
   return _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 }
 
-const composeAnswer = (obj1, obj2, allKeys) => {
-    return allKeys.map((key) => {
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
-    } else if (!_.has(obj1, key)) {
-      return { key, value2: obj2[key], type: 'added' };
-    } else if (!_.has(obj2, key)) {
-      return { key, value1: obj1[key], type: 'deleted' };
-    } 
-    return obj1[key] !== obj2[key] 
+const composeAnswer = (key, obj1, obj2) => {
+  if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+    return { key, children: findDiff(obj1[key], obj2[key]), type: 'nested' };
+  } else if (!_.has(obj1, key)) {
+    return { key, value2: obj2[key], type: 'added' };
+  } else if (!_.has(obj2, key)) {
+    return { key, value1: obj1[key], type: 'deleted' };
+  } 
+  return obj1[key] !== obj2[key] 
     ? { key, value1: obj1[key], value2: obj2[key], type: 'changed', }
     : { key, value1: obj1[key], type: 'unchanged' };
-  });
 }
 
 const findDiff = (obj1, obj2) => {
   const allKeys = sortKeys(obj1, obj2)
   
-  return composeAnswer(obj1, obj2, allKeys)
+  const result = allKeys.map((key) => {
+    return composeAnswer(key, obj1, obj2)
+  });
+  return result;
 };
 
 export default findDiff;
