3fa506df79f4b7bed094134f11b169053f221ab5
Romanov55 <Romanov20let@gmail.com>
2022-11-25 10:06:00 +0600

    up gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,10 +10,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-v, --version', 'output the version number')
   .helpOption('-h, --help', 'display help for command')
-  .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>', 'path to file1')
+  .argument('<filepath2>', 'path to file2')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
