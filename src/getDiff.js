d8fc20731a0455e4c63661d540fb4ca19f230987
Romanov55 <Romanov20let@gmail.com>
2022-11-28 17:44:10 +0600

    up getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const sortKeys = (obj1, obj2) => _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 
-const composeAnswer = (key, obj1, obj2) => {
+const compose = (key, obj1, obj2) => {
   if (!_.has(obj1, key)) {
     return { key, value2: obj2[key], type: 'added' };
   } if (!_.has(obj2, key)) {
@@ -19,10 +19,10 @@ const getDiff = (obj1, obj2) => {
   const allKeys = sortKeys(obj1, obj2);
 
   const result = allKeys.map((key) => {
-    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, children: getDiff(obj1[key], obj2[key]), type: 'nested' };
     }
-    return composeAnswer(key, obj1, obj2);
+    return compose(key, obj1, obj2);
   });
 
   return result;
