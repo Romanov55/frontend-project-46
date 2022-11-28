9ba502fc96c66b15b69699240535d2cabd3042dc
Romanov55 <Romanov20let@gmail.com>
2022-11-28 17:10:57 +0600

    up plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const findValue = (currentValue) => {
+const getStrict = (currentValue) => {
   if (_.isObject(currentValue)) {
     return '[complex value]';
   }
@@ -12,25 +12,35 @@ const findValue = (currentValue) => {
 
 const keyPath = (path, line) => (path === '' ? `${line.key}` : `${path}.${line.key}`);
 
-const composeAnswer = (line, key) => {
-  if (line.type === 'added') {
-    return `Property '${key}' was added with value: ${findValue(line.value2)}`;
-  } if (line.type === 'deleted') {
-    return `Property '${key}' was removed`;
+const compose = (line, key) => {
+  switch (line.type) {
+    case "added":
+      return `Property '${key}' was added with value: ${getStrict(line.value2)}`;
+    case "deleted":
+      return `Property '${key}' was removed`;
+    case "changed":
+      return `Property '${key}' was updated. From ${getStrict(line.value1)} to ${getStrict(line.value2)}`;
+    default:
+      return '';
   }
-  return line.type === 'changed'
-    ? `Property '${key}' was updated. From ${findValue(line.value1)} to ${findValue(line.value2)}`
-    : '';
 };
 
 const plain = (data, path = '') => {
   const lines = data.flatMap((line) => {
     const key = keyPath(path, line);
 
-    if (line.type === 'nested') {
-      return `${plain(line.children, key)}`;
+    switch (line.type) {
+      case 'nested':
+        return `${plain(line.children, key)}`
+      case "added":
+        return `Property '${key}' was added with value: ${getStrict(line.value2)}`;
+      case "deleted":
+        return `Property '${key}' was removed`;
+      case "changed":
+        return `Property '${key}' was updated. From ${getStrict(line.value1)} to ${getStrict(line.value2)}`;
+      default:
+        return '';
     }
-    return composeAnswer(line, key);
   });
 
   return lines.filter((e) => e !== '').join('\n');
