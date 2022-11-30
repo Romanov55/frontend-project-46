f865a2d982d75bc4b7ae7cc16d66a64f0cabb9d5
Romanov55 <Romanov20let@gmail.com>
2022-11-30 15:17:59 +0600

    up index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -18,10 +18,9 @@ test.each([
   ['file1.yml', 'file2.yml', 'plain', 'trueResultPlain.txt'],
   ['file1.json', 'file2.json', 'json', 'trueResultJson.txt'],
   ['file1.yml', 'file2.yml', 'json', 'trueResultJson.txt'],
-])('test message', (filename1, filename2, formatName, expectedName) => {
+])('test', (filename1, filename2, formatName, expectedName) => {
   const file1 = getFixturePath(filename1);
   const file2 = getFixturePath(filename2);
   const expected = fs.readFileSync(getFixturePath(expectedName), 'utf8');
-
   expect(genDiff(file1, file2, formatName)).toEqual(expected);
 });
