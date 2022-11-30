90455da035ac36256b968299358b7e85ecd853ff
Romanov55 <Romanov20let@gmail.com>
2022-11-30 14:10:50 +0600

    up index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,23 +10,23 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-// const file1 = getFixturePath('file1.json');
-// const file2 = getFixturePath('file2.json');
-// const file1yml = getFixturePath('file1.yml');
-// const file2yml = getFixturePath('file2.yml');
-// const expected1 = fs.readFileSync(getFixturePath('trueResultStylish.txt'), 'utf8');
-// const expected2 = fs.readFileSync(getFixturePath('trueResultPlain.txt'), 'utf8');
-// const expected3 = fs.readFileSync(getFixturePath('trueResultJson.txt'), 'utf8');
-
 test.each([
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish', fs.readFileSync(getFixturePath('trueResultStylish.txt'), 'utf8')],
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), fs.readFileSync(getFixturePath('trueResultStylish.txt'), 'utf8')],
-  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish', fs.readFileSync(getFixturePath('trueResultStylish.txt'), 'utf8')],
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain', fs.readFileSync(getFixturePath('trueResultPlain.txt'), 'utf8')],
-  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish', fs.readFileSync(getFixturePath('trueResultPlain.txt'), 'utf8')],
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), 'json', fs.readFileSync(getFixturePath('trueResultJson.txt'), 'utf8')],
-  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json', fs.readFileSync(getFixturePath('trueResultJson.txt'), 'utf8')],
+  [file1, file2, 'stylish', expected1],
+  [file1, file2, expected1],
+  [file1yml, file2yml, 'stylish', expected1],
+  [file1, file2, 'plain', expected2],
+  [file1yml, file2yml, 'stylish', expected2],
+  [file1, file2, 'json', expected3],
+  [file1yml, file2yml, 'json', expected3],
 ])('test message', (filename1, filename2, formatName, expectedName) => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    const file1yml = getFixturePath('file1.yml');
+    const file2yml = getFixturePath('file2.yml');
+
+    const expected1 = fs.readFileSync(getFixturePath('trueResultStylish.txt'), 'utf8');
+    const expected2 = fs.readFileSync(getFixturePath('trueResultPlain.txt'), 'utf8');
+    const expected3 = fs.readFileSync(getFixturePath('trueResultJson.txt'), 'utf8');
   expect(genDiff(filename1, filename2, formatName)).toEqual(expectedName);
 });
 
