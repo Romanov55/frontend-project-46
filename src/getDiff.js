df014fa18cccd21d3d191aa6afe0389f9076eeca
Romanov55 <Romanov20let@gmail.com>
2022-12-02 13:56:14 +0600

    up getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -8,16 +8,17 @@ const getDiff = (obj1, obj2) => {
   const result = allKeys.map((key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, children: getDiff(obj1[key], obj2[key]), type: 'nested' };
-    } if (!_.has(obj1, key)) {
+    }
+    if (!_.has(obj1, key)) {
       return { key, value2: obj2[key], type: 'added' };
-    } if (!_.has(obj2, key)) {
+    }
+    if (!_.has(obj2, key)) {
       return { key, value1: obj1[key], type: 'deleted' };
     }
-    return obj1[key] !== obj2[key]
-      ? {
-        key, value1: obj1[key], value2: obj2[key], type: 'changed',
-      }
-      : { key, value1: obj1[key], type: 'unchanged' };
+    if (obj1[key] !== obj2[key]) {
+      return { key, value1: obj1[key], value2: obj2[key], type: 'changed' }
+    }
+    return { key, value1: obj1[key], type: 'unchanged' };
   });
 
   return result;
