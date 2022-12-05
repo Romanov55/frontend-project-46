063bd7a6c0a150782f28a99e1e6d8c74b2a8ecdb
Romanov55 <Romanov20let@gmail.com>
2022-12-05 20:09:18 +0600

    up parsers

--- src/parsers.js
+++ src/parsers.js
@@ -4,10 +4,12 @@ const getParser = (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
-    case 'yml' || 'yaml':
+    case 'yaml':
+      return yaml.load(data);
+    case 'yml':
       return yaml.load(data);
     default:
-      throw new Error`Unknown format - ${format}`();
+      throw new Error(`Unknown format - ${format}`);
   }
 };
 
